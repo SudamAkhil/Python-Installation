064b326e92739464ba50f629bfe6a8bd282c4b2f
SudamAkhil <99243562+SudamAkhil@users.noreply.github.com>
2022-02-23 14:05:47 +0530

    Add files via upload

---
+++ Introduction of Math/Math.py
@@ -0,0 +1,31 @@
+import math
+
+'''ceil'''
+# print(math.ceil(0.1))
+# print(math.ceil(2.4))
+# print(math.ceil(2.0))
+# print(math.ceil(-6.3))
+
+'''factorial'''
+
+# print(math.factorial(0))
+# print(math.factorial(1))
+# print(math.factorial(2))
+# print(math.factorial(3))
+# print(math.factorial(4))
+# print(math.factorial(5))
+
+'''Floor'''
+
+# print(math.floor(23))
+# print(math.floor(23.8))
+# print(math.floor(28.0))
+# print(math.floor(-3.8))
+# print(math.floor(-3.0))
+
+'''power'''
+
+# a=math.pow(2,3)
+# b=2**3
+# print(a,type(a))
+# print(b,type(b))
