6a4a217031cfe37b2757a827d7307bd18c8404bc
SudamAkhil <99243562+SudamAkhil@users.noreply.github.com>
2022-03-11 21:05:03 +0530

    Add files via upload

---
+++ Strings Methods/strings2.py
@@ -0,0 +1,48 @@
+'''Expand tabs'''
+
+# s="T\tV\tS"
+# print(s.expandtabs(0))
+# print(s.expandtabs(1))
+# print(s.expandtabs(2))
+# print(s.expandtabs(3))
+# print(s.expandtabs(4))
+# print(s.expandtabs(5))
+# print(s.expandtabs(6))
+# print(s.expandtabs(7))
+# print(s.expandtabs(8))
+# print(s.expandtabs(9))
+# print(s.expandtabs(12))
+# print(s.expandtabs(24))
+# print(s.expandtabs())
+# print(s.expandtabs(-1))
+# print(s.expandtabs(-12))
+# print(s.expandtabs(-100))
+
+'''join'''
+
+# S='Welcome','to','my','World'
+# A='programming'
+# print('-'.join(S))
+# print('-'.join(A))
+# print('*'.join("programmig"))
+
+'''Maketrans and Translate'''
+
+# K="Sudem Krishna"
+# S=K.maketrans('e','a')
+# print(S,type(S))
+# print(K.translate(S))
+
+# s="sidam manemma"
+# a=s.maketrans('','u') # ValueError
+# print(s.translate(a))
+
+# s="sidam manemma"
+# a=s.maketrans(' ','u') 
+# print(s.translate(a))
+
+# q={}
+# print(q,type(q))
+# k={1:'welcome',2:'to',3:'my',4:'world'}
+# print(k[2])
+# print(k[4])
